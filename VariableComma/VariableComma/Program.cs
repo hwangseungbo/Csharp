831915800a2e00f6072b8b49971293b8f4aed654
hwangseungbo <66284568+hwangseungbo@users.noreply.github.com>
2020-09-08 17:10:35 +0900

    dd

---
+++ VariableComma/VariableComma/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariableComma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, number3;
+
+            number1 = 1;
+            number2 = 2;
+            number3 = 3;
+
+            Console.WriteLine("{0}, {1}, {2}", number1, number2, number3);
+        }
+    }
+}
