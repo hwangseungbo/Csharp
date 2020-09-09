1e3382c79ca2634508f1188711272aca285b57e5
hwangseungbo <66284568+hwangseungbo@users.noreply.github.com>
2020-09-09 09:27:22 +0900

    dd

---
+++ ConstantDemo/ConstantDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConstantDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           const int MAX = 100;
+           Console.WriteLine("최댓값 : {0}", MAX);
+        }
+    }
+}
