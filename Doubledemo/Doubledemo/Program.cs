a55ff8ca9e71c7b12969ea511785b757a8f957d9
hwangseungbo <66284568+hwangseungbo@users.noreply.github.com>
2020-09-10 15:15:25 +0900

    Double Float Demo

---
+++ Doubledemo/Doubledemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Doubledemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float f = Single.MaxValue;
+            double d = Double.MaxValue;
+            double dd = 3.14d;
+
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+        }
+    }
+}
