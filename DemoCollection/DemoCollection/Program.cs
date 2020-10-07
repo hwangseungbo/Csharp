b9d68e76fad67578fc41213e88db6008bce361ed
hwangseungbo <66284568+hwangseungbo@users.noreply.github.com>
2020-10-07 11:19:33 +0900

    renew 10/7

--- DemoCollection/DemoCollection/Program.cs
+++ DemoCollection/DemoCollection/Program.cs
@@ -200,6 +200,37 @@ namespace DemoCollection
             //    Console.WriteLine("변환 불가");
             //}
 
+            //int x = 2;
+            //switch (x)
+            //{
+            //    case 1:
+            //        Console.WriteLine("1입니다.");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("2입니다.");
+            //        break;
+            //}
+
+            //Console.WriteLine("정수를 입력하세요");
+            //int answer = Convert.ToInt32(Console.ReadLine());
+            //switch (answer)
+            //{
+            //    case 1:
+            //        Console.WriteLine("1을 선택하셨습니다.");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("2을 선택하셨습니다.");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("3을 선택하셨습니다.");
+            //        break;
+            //    default :
+            //        Console.WriteLine("그냥 찍으셨군요.");
+            //        break;
+            //}
+           
+
+
         }
     }
 }
