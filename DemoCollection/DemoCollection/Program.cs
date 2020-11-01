f68e6daaf12f6ca04ae5894ebcac001c3d6dfc99
hwangseungbo <66284568+hwangseungbo@users.noreply.github.com>
2020-11-01 11:45:41 +0900

    renew 11/1

--- DemoCollection/DemoCollection/Program.cs
+++ DemoCollection/DemoCollection/Program.cs
@@ -284,6 +284,7 @@ namespace DemoCollection
             //Console.WriteLine("10/29");
             //Console.WriteLine("10/30");
             //Console.WriteLine("10/31");
+            //Console.WriteLine("11/1");
         }
     }
 }
