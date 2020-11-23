f0ff1ca7806f64a117bbe1a194ae164fe8f0b35b
hwangseungbo <66284568+hwangseungbo@users.noreply.github.com>
2020-11-23 09:17:46 +0900

    renew 11/23

--- DemoCollection/DemoCollection/Program.cs
+++ DemoCollection/DemoCollection/Program.cs
@@ -306,6 +306,20 @@ namespace DemoCollection
             //Console.WriteLine("11/20");
             //Console.WriteLine("11/21");
             //Console.WriteLine("11/22");
+
+            //int[] kor = new int[3];
+            //int sum = 0;
+            //float avg = 0;
+
+            //kor[0] = 100;
+            //kor[1] = 90;
+            //kor[2] = 80;
+
+            //sum = kor[0] + kor[1] + kor[2];
+            //avg = sum / (float)3.0;
+
+            //Console.WriteLine($"총점 : {sum}, 평균 : {avg:0.00}");
+
         }
     }
 }
