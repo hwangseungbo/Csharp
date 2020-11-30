cbf59bbcaec0de6c97e04bd1aa27350fc0a37a98
hwangseungbo <66284568+hwangseungbo@users.noreply.github.com>
2020-11-30 11:48:25 +0900

    renew 11/30

--- DemoCollection/DemoCollection/Program.cs
+++ DemoCollection/DemoCollection/Program.cs
@@ -335,6 +335,16 @@ namespace DemoCollection
             //Console.WriteLine("11/28");
             //Console.WriteLine("11/29");
 
+            //int[] students = new int[3];
+
+            //students[0] = Convert.ToInt32(Console.ReadLine());
+            //students[1] = Convert.ToInt32(Console.ReadLine());
+            //students[2] = Convert.ToInt32(Console.ReadLine());
+
+            //int total = students[0] + students[1] + students[2];
+            //Console.WriteLine($"총점 : {total:00.00}");
+
+
 
 
 
