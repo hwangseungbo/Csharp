084ba4ebba049bef17471e7b6e22b1990e3ed37b
hwangseungbo <66284568+hwangseungbo@users.noreply.github.com>
2021-01-12 09:06:33 +0900

    renew 1/12

--- DemoCollection/DemoCollection/Program.cs
+++ DemoCollection/DemoCollection/Program.cs
@@ -437,6 +437,7 @@ namespace DemoCollection
             //Console.WriteLine("1/9);
             //Console.WriteLine("1/10);
             //Console.WriteLine("1/11);
+            //Console.WriteLine("1/12);
 
 
 
