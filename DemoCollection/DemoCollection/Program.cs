31f9f14d67f26eae9a4f5c1836141665dcbedccd
hwangseungbo <66284568+hwangseungbo@users.noreply.github.com>
2021-01-27 09:29:28 +0900

    renew 1/27

--- DemoCollection/DemoCollection/Program.cs
+++ DemoCollection/DemoCollection/Program.cs
@@ -452,6 +452,7 @@ namespace DemoCollection
             //Console.WriteLine("1/24);
             //Console.WriteLine("1/25);
             //Console.WriteLine("1/26);
+            //Console.WriteLine("1/27);
 
 
 
