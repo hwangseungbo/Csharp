c5d40cc887bf72ebb454d0454aab17473646bee8
hwangseungbo <66284568+hwangseungbo@users.noreply.github.com>
2021-02-10 17:07:38 +0900

    renew 2/10

--- DemoCollection/DemoCollection/Program.cs
+++ DemoCollection/DemoCollection/Program.cs
@@ -11,32 +11,7 @@ namespace DemoCollection
     {
         static void Main(string[] args)
         {
-            //String Address = Console.ReadLine();
-            //IPAddress IP = IPAddress.Parse(Address);
-            //Console.WriteLine("ip : {0}", IP.ToString());
-            //IPAddress[] IP = Dns.GetHostAddresses("www.naver.com");
-            //foreach(IPAddress HostIP in IP)
-            //{
-            //    Console.WriteLine("{0} ", HostIP);
-            //}
-
-
-
-            //IPHostEntry HostInfo = Dns.GetHostEntry("www.naver.com");
-
-            //foreach (IPAddress ip in HostInfo.AddressList)
-            //{
-            //    Console.WriteLine("{0}", ip);
-            //}
-
-            //Console.WriteLine("{0}", HostInfo.HostName);
-
-            IPAddress IPInfo = IPAddress.Parse("127.0.0.1");
-            int Port = 13;
-            IPEndPoint EndPointInfo = new IPEndPoint(IPInfo, Port);
-            Console.WriteLine("ip: {0}    port: {1}", EndPointInfo.Address, EndPointInfo.Port);
-            Console.WriteLine(EndPointInfo.ToString());
-            Console.ReadKey();
+            Console.WriteLine("2/10");
         }
     }
 }
